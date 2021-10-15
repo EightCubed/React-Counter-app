e3dcde04df62aee894d86230d77447b537b05728
Rkn301 <sushyam.s@gmail.com>
2021-10-15 16:24:41 +0530

    added button functionality and created lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count : 0
+        count : 0,
+        tags : ["tag1","tag2","tag3"]
     };
+
+    buttonIncrement = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
 
     render() { 
         return (
             <div>
                 <span className={this.buttonDynamic()} >{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ this.buttonIncrement } className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map ( tag => <li key={tag}> {tag} </li> ) }
+                </ul>
             </div>
         );
     }

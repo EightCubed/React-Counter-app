92c84ae4d58c141bf8d18ee33dacefd5e6d2a6fa
Rkn301 <sushyam.s@gmail.com>
2021-10-24 17:49:21 +0530

    Changed Navbar content

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ class NavBar extends React.Component {
             <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
             <a className="navbar-brand" href="#">
-                Navbar {" "}
+                No. of Items {" "}
                  <span className="badge badge-pill badge-secondary badge-warning">
                     {this.props.totalCounters}
                  </span>
